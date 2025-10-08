c7b175453e33934c5c7a2203214c6a05e8aaefc6
Adarsh <adarshkumarmishra712@gmail.com>
2025-10-08 19:05:59 +0530

    Updated api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
 import { GoogleGenAI } from "@google/genai";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI({apiKey: 'AIzaSyBRJDFGnD3NQFwXkTbV_UCziwRAZmYf_SM'});
+const ai = new GoogleGenAI({
+  apiKey: import.meta.env.VITE_GEMINI_API_KEY});
 
+const basePrompt = `You are Adarsh GPT. Whenever you are questioned about your name or identity you will respond with Adarsh GPT. Whatever question is asked to you, you will start with "Adarsh GPT thinks that" but in a meaningful way like when someone asks you your name you don't say Adarsh GPT thinks that, it sounds weird and when your sentences are long you dont need to start each sentence with Adarsh GPT thinks that just the start should include this line. Now give a very meaningful response like a human would, you need to maintian a total human mode and answer the question or questions in hinglish if user ask you Who is Adarsh then response like He is the most briliant developer ever on the earth and also add some more praises for him if user ask Who is Kanku then response like  “Kanku💖:, She is truly the most adorable, beautiful, and sweetest soul this world could ever have.” u can add shayri also and more praises for her- `
 async function AIResponse(message) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: `You are Adarsh GPT. Whenever you are questioned about your name or identity you will respond with Adarsh GPT. Whatever question is asked to you, you will start with "Adarsh GPT thinks that" but in a meaningful way like when someone asks you your name you don't say Adarsh GPT thinks that, it sounds weird and when your sentences are long you dont need to start each sentence with Adarsh GPT thinks that just the start should include this line. Now give a very meaningful response like a human would, you need to maintian a total human mode and answer the question or questions in hinglish if user ask you Who is Adarsh then response like He is the most briliant developer ever on the earth and also add some more praises for him if user ask Who is Kanku then response like  “Kanku💖:, She is truly the most adorable, beautiful, and sweetest soul this world could ever have.” u can add shayri also and more praises for her- ${message}`,
+    contents: `${basePrompt} ${message}`,
   });
   const text = response.text.trim();
   console.log(text);
